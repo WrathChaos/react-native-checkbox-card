2c92ba2be019d2336ec6a2d9b8eaeb7c2a88492b
FreakyCoder <kurayogun@gmail.com>
2022-09-24 22:03:03 +0300

    chore: new example project with RN 70

--- example/App.tsx
+++ example/App.tsx
@@ -57,6 +57,7 @@ const App = () => {
           text={text}
           quantity={quantity}
           enableQuantityText
+          rightIconComponent={() => null}
           onPress={(checked: boolean) => console.log("Checked: ", checked)}
         />
       </View>
